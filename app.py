5ab7d94343c2c6bb4070886e7b83a9cfaef5595a
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-03-19 17:44:04 -0400

    Drafts first proof of concept

--- app.py
+++ app.py
@@ -1,13 +1,55 @@
 from flask import Flask, request, send_file
+from google.cloud import speech
+from docx import Document
+import io
+
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def upload_file():
-    file = request.files['file']
-    if file:
-        return "Audio file received", 200
-    
+    print(request.files)
+    if "file" not in request.files:
+        return "No file part", 400
+    file = request.files["file"]
 
-if __name__ == '__main__':
+    if file.filename == "":
+        return "No selected file", 400
+
+    if file:
+        content = file.read()
+        transcription = transcribe_audio(content)
+        return create_word_document(transcription)
+
+
+def transcribe_audio(content):
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=44100,
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    transcription = ""
+    for result in response.results:
+        transcription += result.alternatives[0].transcript + "\n"
+
+    return transcription
+
+
+def create_word_document(transcription):
+    document = Document()
+    document.add_heading("Reading Title", 0)
+    document.add_heading("A Commentary", level=2)
+    document.add_paragraph(transcription)
+
+    document.save("transcription.docx")
+
+    return send_file("transcription.docx", as_attachment=True)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-    
