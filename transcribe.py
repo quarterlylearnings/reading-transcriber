012aa7735d0a676124daae52ece5621873effa96
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-04-09 20:53:46 -0400

    Fixes incorrect config key

--- transcribe.py
+++ transcribe.py
@@ -11,13 +11,13 @@ def transcribe_gcs_audio_file(gcs_uri, filename):
         min_speaker_count=1,
         max_speaker_count=4,
     )
-    
+
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=44100,
         language_code="en-US",
         enable_automatic_punctuation=True,
-        speaker_diarization_config=speaker_diarization_config,
+        diarization_config=speaker_diarization_config,
     )
 
     operation = client.long_running_recognize(config=config, audio=audio)
