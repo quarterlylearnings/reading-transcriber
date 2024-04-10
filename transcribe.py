15bb5702928ea127c0c9d1f4d848a99a89a6c2bb
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-04-09 20:41:39 -0400

    Adds speaker diarization config

--- transcribe.py
+++ transcribe.py
@@ -3,13 +3,21 @@ import os
 
 def transcribe_gcs_audio_file(gcs_uri, filename):
     client = speech.SpeechClient()
-    
+
     audio = speech.RecognitionAudio(uri=gcs_uri)
+
+    speaker_diarization_config = speech.SpeakerDiarizationConfig(
+        enable_speaker_diarization=True,
+        min_speaker_count=1,
+        max_speaker_count=4,
+    )
+    
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
         sample_rate_hertz=44100,
         language_code="en-US",
-        enable_automatic_punctuation=True
+        enable_automatic_punctuation=True,
+        speaker_diarization_config=speaker_diarization_config,
     )
 
     operation = client.long_running_recognize(config=config, audio=audio)
