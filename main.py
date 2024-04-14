15b395b002f616949d767fad2f7cacf51fca98ca
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-04-14 18:18:35 -0400

    Renames root to main

--- main.py
+++ main.py
@@ -43,5 +43,3 @@ def upload_file():
         transcription = transcribe_gcs_audio_file(uri, filename)
         return create_word_document(transcription, filename)
  
-if __name__ == "__main__":
-    app.run()
