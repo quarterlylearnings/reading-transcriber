8bf6c8a253d5bcd527fc3e90eee064718c798e04
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2024-04-22 15:24:45 -0400

    Modifies transcription operation to save result to specified storage bucket

--- transcribe.py
+++ transcribe.py
@@ -19,7 +19,12 @@ def transcribe_gcs_audio_file(gcs_uri, filename):
         diarization_config=speaker_diarization_config,
     )
 
-    operation = client.long_running_recognize(config=config, audio=audio)
+    output_config = speech.TranscriptOutputConfig(
+        gcs_uri=f"gs://{os.getenv('GCS_TRANSCRIPTION_BUCKET_NAME')}/{filename}",
+        enable_automatic_punctuation=True,
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio, output_config=output_config)
     print("Waiting for operation to complete...")
 
     return operation.running()
